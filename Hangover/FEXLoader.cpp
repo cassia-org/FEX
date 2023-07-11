8a591676ad5d180cfbcdff5d4e7db81cfc26ad53
Billy Laws <blaws05@gmail.com>
2023-07-11 13:19:00 +0100

    Hangover: Split out context save/restore code

--- Hangover/FEXLoader.cpp
+++ Hangover/FEXLoader.cpp
@@ -86,16 +86,107 @@ extern "C" __attribute__((visibility ("default"))) void ho_A() {
 static thread_local FEXCore::Core::InternalThreadState *Thread;
 static FEXCore::Core::InternalThreadState *MainThread;
 
-extern "C" __attribute__((visibility ("default"))) void ho_B(uint64_t wowteb, I386_CONTEXT* ctx)
-{
-  if (!MainThread)
-  {
-    Thread = CTX->InitCore(ctx->Eip, ctx->Esp);
+static void LoadStateFromWinContext(FEXCore::Core::CPUState& State, uint64_t WowTeb, I386_CONTEXT* Context) {
+  // General register state
+
+  State.gregs[FEXCore::X86State::REG_RAX] = Context->Eax;
+  State.gregs[FEXCore::X86State::REG_RBX] = Context->Ebx;
+  State.gregs[FEXCore::X86State::REG_RCX] = Context->Ecx;
+  State.gregs[FEXCore::X86State::REG_RDX] = Context->Edx;
+  State.gregs[FEXCore::X86State::REG_RSI] = Context->Esi;
+  State.gregs[FEXCore::X86State::REG_RDI] = Context->Edi;
+  State.gregs[FEXCore::X86State::REG_RBP] = Context->Ebp;
+  State.gregs[FEXCore::X86State::REG_RSP] = Context->Esp;
+
+  State.rip = Context->Eip;
+
+  for (size_t i = 0; i < FEXCore::Core::CPUState::NUM_EFLAG_BITS; ++i) {
+    State.flags[i] = (Context->EFlags >> i) & 1;
+  }
+
+  State.es_idx = Context->SegEs & 0xFFFF;
+  State.cs_idx = Context->SegCs & 0xFFFF;
+  State.ss_idx = Context->SegSs & 0xFFFF;
+  State.ds_idx = Context->SegDs & 0xFFFF;
+  State.fs_idx = Context->SegFs & 0xFFFF;
+  State.gs_idx = Context->SegGs & 0xFFFF;
+
+  // The TEB is the only populated GDT entry by default
+  State.gdt[(Context->SegFs & 0xFFFF) >> 3].base = WowTeb;
+  State.fs_cached = WowTeb;
+  State.es_cached = 0;
+  State.cs_cached = 0;
+  State.ss_cached = 0;
+  State.ds_cached = 0;
+
+  // Floating-point register state
+
+  auto* XSave = reinterpret_cast<XSAVE_FORMAT*>(Context->ExtendedRegisters);
+
+  memcpy(State.xmm.sse.data, XSave->XmmRegisters, sizeof(State.xmm.sse.data));
+  memcpy(State.mm, XSave->FloatRegisters, sizeof(State.mm));
+
+  State.FCW = XSave->ControlWord;
+  State.flags[FEXCore::X86State::X87FLAG_C0_LOC] = (XSave->StatusWord >> 8) & 1;
+  State.flags[FEXCore::X86State::X87FLAG_C1_LOC] = (XSave->StatusWord >> 9) & 1;
+  State.flags[FEXCore::X86State::X87FLAG_C2_LOC] = (XSave->StatusWord >> 10) & 1;
+  State.flags[FEXCore::X86State::X87FLAG_C3_LOC] = (XSave->StatusWord >> 14) & 1;
+  State.flags[FEXCore::X86State::X87FLAG_TOP_LOC] = (XSave->StatusWord >> 11) & 0b111;
+  State.FTW = XSave->TagWord;
+}
+
+static void StoreWinContextFromState(FEXCore::Core::CPUState& State, uint64_t WowTeb, I386_CONTEXT* Context) {
+  // General register state
+
+  Context->Eax = State.gregs[FEXCore::X86State::REG_RAX];
+  Context->Ebx = State.gregs[FEXCore::X86State::REG_RBX];
+  Context->Ecx = State.gregs[FEXCore::X86State::REG_RCX];
+  Context->Edx = State.gregs[FEXCore::X86State::REG_RDX];
+  Context->Esi = State.gregs[FEXCore::X86State::REG_RSI];
+  Context->Edi = State.gregs[FEXCore::X86State::REG_RDI];
+  Context->Ebp = State.gregs[FEXCore::X86State::REG_RBP];
+  Context->Esp = State.gregs[FEXCore::X86State::REG_RSP];
+
+  Context->Eip = State.rip;
+
+  Context->EFlags = 0;
+  for (size_t i = 0; i < FEXCore::Core::CPUState::NUM_EFLAG_BITS; ++i) {
+      Context->EFlags |= State.flags[i] << i;
+  }
+
+  Context->SegEs = State.es_idx;
+  Context->SegCs = State.cs_idx;
+  Context->SegSs = State.ss_idx;
+  Context->SegDs = State.ds_idx;
+  Context->SegFs = State.fs_idx;
+  Context->SegGs = State.gs_idx;
+
+  // Floating-point register state
+
+  auto* XSave = reinterpret_cast<XSAVE_FORMAT*>(Context->ExtendedRegisters);
+
+  memcpy(XSave->XmmRegisters, State.xmm.sse.data, sizeof(State.xmm.sse.data));
+  memcpy(XSave->FloatRegisters, State.mm, sizeof(State.mm));
+
+  XSave->ControlWord = State.FCW;
+  XSave->StatusWord =
+    (State.flags[FEXCore::X86State::X87FLAG_TOP_LOC] << 11) |
+    (State.flags[FEXCore::X86State::X87FLAG_C0_LOC] << 8) |
+    (State.flags[FEXCore::X86State::X87FLAG_C1_LOC] << 9) |
+    (State.flags[FEXCore::X86State::X87FLAG_C2_LOC] << 10) |
+    (State.flags[FEXCore::X86State::X87FLAG_C3_LOC] << 14);
+  XSave->TagWord = State.FTW;
+
+  fpux_to_fpu(&Context->FloatSave, XSave);
+}
+
+extern "C" __attribute__((visibility ("default"))) void ho_B(uint64_t WowTeb, I386_CONTEXT* Context) {
+  if (!MainThread) {
+    Thread = CTX->InitCore(Context->Eip, Context->Esp);
     MainThread = Thread;
   }
 
-  if (!Thread)
-  {
+  if (!Thread) {
     FEXCore::Core::CPUState NewThreadState{};
     memset(NewThreadState.gdt, 0, sizeof(FEXCore::Core::CPUState::gdt));
     NewThreadState.es_cached = NewThreadState.cs_cached = NewThreadState.ss_cached = NewThreadState.ds_cached = NewThreadState.gs_cached = NewThreadState.fs_cached = 0;
@@ -103,142 +194,17 @@ extern "C" __attribute__((visibility ("default"))) void ho_B(uint64_t wowteb, I3
     Thread->DestroyedByParent = 1;
   }
 
-  if (ctx)
-  {
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RAX] = ctx->Eax;
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RBX] = ctx->Ebx;
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RCX] = ctx->Ecx;
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RDX] = ctx->Edx;
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RSI] = ctx->Esi;
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RDI] = ctx->Edi;
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RBP] = ctx->Ebp;
-    Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RSP] = ctx->Esp;
+  static constexpr uint32_t RequiredContextFlags = CONTEXT_I386_FULL | CONTEXT_I386_DEBUG_REGISTERS;
 
-    Thread->CurrentFrame->State.rip = ctx->Eip;
-
-    /* flags? */
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_CF_LOC] = ctx->EFlags & 1;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_PF_LOC] = ctx->EFlags & 4;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_AF_LOC] = ctx->EFlags & 0x10;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_ZF_LOC] = ctx->EFlags & 0x40;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_SF_LOC] = ctx->EFlags & 0x80;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_TF_LOC] = ctx->EFlags & 0x100;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_IF_LOC] = ctx->EFlags & 0x200;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_DF_LOC] = ctx->EFlags & 0x400;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_OF_LOC] = ctx->EFlags & 0x800;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_NT_LOC] = ctx->EFlags & 0x4000;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_RF_LOC] = ctx->EFlags & 0x10000;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_VM_LOC] = ctx->EFlags & 0x20000;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_AC_LOC] = ctx->EFlags & 0x40000;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_VIF_LOC] = ctx->EFlags & 0x80000;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_VIP_LOC] = ctx->EFlags & 0x100000;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_ID_LOC] = ctx->EFlags & 0x200000;
-
-    Thread->CurrentFrame->State.es_idx = ctx->SegEs & 0xFFFF;
-    Thread->CurrentFrame->State.cs_idx = ctx->SegCs & 0xFFFF;
-    Thread->CurrentFrame->State.ss_idx = ctx->SegSs & 0xFFFF;
-    Thread->CurrentFrame->State.ds_idx = ctx->SegDs & 0xFFFF;
-    Thread->CurrentFrame->State.fs_idx = ctx->SegFs & 0xFFFF;
-    Thread->CurrentFrame->State.gs_idx = ctx->SegGs & 0xFFFF;
-
-    // The TEB is the only populated GDT entry by default
-    Thread->CurrentFrame->State.gdt[(ctx->SegFs & 0xFFFF) >> 3].base = wowteb;
-    Thread->CurrentFrame->State.fs_cached = wowteb;
-    Thread->CurrentFrame->State.es_cached = 0;
-    Thread->CurrentFrame->State.cs_cached = 0;
-    Thread->CurrentFrame->State.ss_cached = 0;
-    Thread->CurrentFrame->State.ds_cached = 0;
-    /*debug regs*/
-    /*float*/
-    memcpy(&Thread->CurrentFrame->State.mm[0], &ctx->FloatSave.RegisterArea[0],  10);
-    memcpy(&Thread->CurrentFrame->State.mm[1], &ctx->FloatSave.RegisterArea[10], 10);
-    memcpy(&Thread->CurrentFrame->State.mm[2], &ctx->FloatSave.RegisterArea[20], 10);
-    memcpy(&Thread->CurrentFrame->State.mm[3], &ctx->FloatSave.RegisterArea[30], 10);
-    memcpy(&Thread->CurrentFrame->State.mm[4], &ctx->FloatSave.RegisterArea[40], 10);
-    memcpy(&Thread->CurrentFrame->State.mm[5], &ctx->FloatSave.RegisterArea[50], 10);
-    memcpy(&Thread->CurrentFrame->State.mm[6], &ctx->FloatSave.RegisterArea[60], 10);
-    memcpy(&Thread->CurrentFrame->State.mm[7], &ctx->FloatSave.RegisterArea[70], 10);
-
-    Thread->CurrentFrame->State.FCW = ctx->FloatSave.ControlWord;
-    Thread->CurrentFrame->State.FTW = ctx->FloatSave.TagWord;
-    auto *win32_xstate = reinterpret_cast<XSAVE_FORMAT*>(ctx->ExtendedRegisters);
-    memcpy(Thread->CurrentFrame->State.xmm.sse.data, win32_xstate->XmmRegisters, sizeof(Thread->CurrentFrame->State.xmm.sse.data));
-    Thread->CurrentFrame->State.FCW = win32_xstate->ControlWord;
-    Thread->CurrentFrame->State.FTW = win32_xstate->TagWord;
-
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C0_LOC] = (ctx->FloatSave.StatusWord >> 8) & 1;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C1_LOC] = (ctx->FloatSave.StatusWord >> 9) & 1;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C2_LOC] = (ctx->FloatSave.StatusWord >> 10) & 1;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C3_LOC] = (ctx->FloatSave.StatusWord >> 14) & 1;
-    Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_TOP_LOC] = (ctx->FloatSave.StatusWord >> 11) & 0b111;
+  if ((Context->ContextFlags & RequiredContextFlags) != RequiredContextFlags) {
+    fprintf(stderr, "Incomplete context!\n");
   }
+
+  LoadStateFromWinContext(Thread->CurrentFrame->State, WowTeb, Context);
 
   CTX->ExecutionThread(Thread);
 
-  if (ctx)
-  {
-    ctx->Eax = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RAX];
-    ctx->Ebx = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RBX];
-    ctx->Ecx = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RCX];
-    ctx->Edx = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RDX];
-    ctx->Esi = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RSI];
-    ctx->Edi = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RDI];
-    ctx->Ebp = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RBP];
-    ctx->Esp = Thread->CurrentFrame->State.gregs[FEXCore::X86State::REG_RSP];
-
-    ctx->Eip = Thread->CurrentFrame->State.rip;
-
-    /* flags? */
-    ctx->EFlags = 0;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_CF_LOC]) << 0;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_PF_LOC]) << 2;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_AF_LOC]) << 4;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_ZF_LOC]) << 6;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_SF_LOC]) << 7;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_TF_LOC]) << 8;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_IF_LOC]) << 9;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_DF_LOC]) << 10;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_OF_LOC]) << 11;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_NT_LOC] ) << 14;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_RF_LOC] ) << 16;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_VM_LOC] ) << 17;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_AC_LOC] ) << 18;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_VIF_LOC]) << 19;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_VIP_LOC]) << 20;
-    ctx->EFlags |= (!!Thread->CurrentFrame->State.flags[FEXCore::X86State::RFLAG_ID_LOC] ) << 21;
-
-    ctx->SegEs = Thread->CurrentFrame->State.es_idx;
-    ctx->SegCs = Thread->CurrentFrame->State.cs_idx;
-    ctx->SegSs = Thread->CurrentFrame->State.ss_idx;
-    ctx->SegDs = Thread->CurrentFrame->State.ds_idx;
-    ctx->SegFs = Thread->CurrentFrame->State.fs_idx;
-    ctx->SegGs = Thread->CurrentFrame->State.gs_idx;
-
-    /*debug regs*/
-    /*float*/
-    memcpy(&ctx->FloatSave.RegisterArea[0],  &Thread->CurrentFrame->State.mm[0], 10);
-    memcpy(&ctx->FloatSave.RegisterArea[10], &Thread->CurrentFrame->State.mm[1], 10);
-    memcpy(&ctx->FloatSave.RegisterArea[20], &Thread->CurrentFrame->State.mm[2], 10);
-    memcpy(&ctx->FloatSave.RegisterArea[30], &Thread->CurrentFrame->State.mm[3], 10);
-    memcpy(&ctx->FloatSave.RegisterArea[40], &Thread->CurrentFrame->State.mm[4], 10);
-    memcpy(&ctx->FloatSave.RegisterArea[50], &Thread->CurrentFrame->State.mm[5], 10);
-    memcpy(&ctx->FloatSave.RegisterArea[60], &Thread->CurrentFrame->State.mm[6], 10);
-    memcpy(&ctx->FloatSave.RegisterArea[70], &Thread->CurrentFrame->State.mm[7], 10);
-
-    ctx->FloatSave.ControlWord = Thread->CurrentFrame->State.FCW;
-    ctx->FloatSave.TagWord = Thread->CurrentFrame->State.FTW;
-    auto *win32_xstate = reinterpret_cast<XSAVE_FORMAT*>(ctx->ExtendedRegisters);
-    memcpy(win32_xstate->XmmRegisters, Thread->CurrentFrame->State.xmm.sse.data, sizeof(Thread->CurrentFrame->State.xmm.sse.data));
-    win32_xstate->ControlWord = Thread->CurrentFrame->State.FCW;
-    win32_xstate->TagWord = Thread->CurrentFrame->State.FTW;
-
-    ctx->FloatSave.StatusWord =
-      (Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_TOP_LOC] << 11) |
-      (Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C0_LOC] << 8) |
-      (Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C1_LOC] << 9) |
-      (Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C2_LOC] << 10) |
-      (Thread->CurrentFrame->State.flags[FEXCore::X86State::X87FLAG_C3_LOC] << 14);
-  }
+  StoreWinContextFromState(Thread->CurrentFrame->State, WowTeb, Context);
 }
 
 extern "C" __attribute__((visibility ("default"))) void ho_invalidate_code_range(uint64_t Start, uint64_t Length) {
